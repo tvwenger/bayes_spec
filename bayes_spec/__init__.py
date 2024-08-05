28e3e01b31f0e762e9bbc20eb6dc42fe85fee060
Trey Wenger <tvwenger@gmail.com>
2024-08-04 23:29:24 -0500

    update to versioneer

--- bayes_spec/__init__.py
+++ bayes_spec/__init__.py
@@ -1,4 +1,3 @@
-__version__ = "1.3.1"
 __all__ = [
     "SpecData",
     "BaseModel",
@@ -8,3 +7,6 @@ __all__ = [
 from bayes_spec.spec_data import SpecData
 from bayes_spec.base_model import BaseModel
 from bayes_spec.optimize import Optimize
+
+from . import _version
+__version__ = _version.get_versions()['version']
--- setup.py
+++ setup.py
@@ -1,25 +1,17 @@
-from setuptools import setup
-import re
-
-
-def get_property(prop, project):
-    result = re.search(
-        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
-        open(project + "/__init__.py").read(),
-    )
-    return result.group(1)
-
+from setuptools import find_packages, setup
+import versioneer
 
 with open("requirements.txt") as f:
     install_requires = f.read().splitlines()
 
 setup(
     name="bayes_spec",
-    version=get_property("__version__", "bayes_spec"),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description="A Bayesian Spectral Line Modeling Framework for Astrophysics",
     author="Trey V. Wenger",
     author_email="tvwenger@gmail.com",
-    packages=["bayes_spec"],
+    packages=find_packages(),
     install_requires=install_requires,
     python_requires=">=3.9",
     license="GNU GPLv3",
