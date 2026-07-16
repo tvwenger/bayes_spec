1ddf15354fb263941ef657c86242cbe919b26fe7
Trey Wenger <tvwenger@gmail.com>
2026-07-16 12:02:25 -0700

    update tests

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -36,7 +36,15 @@ def test_fit_all():
     opt.add_priors()
     opt.add_likelihood()
     start_spread = {"velocity_norm": [-3.0, 3.0]}
-    opt.fit_all(start_spread=start_spread)
+    opt.fit_all(
+        start_spread=start_spread,
+        **{
+            "n": 1000,
+            "rel_tolerance": 0.01,
+            "abs_tolerance": 0.1,
+            "learning_rate": 1e-2,
+        },
+    )
 
 
 def test_sample_all():
@@ -45,7 +53,22 @@ def test_sample_all():
     opt.add_priors()
     opt.add_likelihood()
     start_spread = {"velocity_norm": [-3.0, 3.0]}
-    opt.sample_all(start_spread=start_spread)
+    fit_kwargs = {
+        "rel_tolerance": 0.01,
+        "abs_tolerance": 0.1,
+        "learning_rate": 1e-2,
+    }
+    opt.sample_all(
+        start_spread=start_spread,
+        sample_kwargs={
+            "tune": 100,
+            "draws": 100,
+            "chains": 2,
+            "cores": 2,
+            "n_init": 1000,
+            "init_kwargs": fit_kwargs,
+        },
+    )
 
 
 def test_sample_smc_all():
@@ -53,7 +76,7 @@ def test_sample_smc_all():
     opt = Optimize(GaussModel, _DATA, max_n_clouds=1, verbose=True)
     opt.add_priors()
     opt.add_likelihood()
-    opt.sample_smc_all()
+    opt.sample_smc_all(sample_kwargs={"chains": 2, "draws": 100})
 
 
 def test_optimize_vi_mcmc():
@@ -62,10 +85,28 @@ def test_optimize_vi_mcmc():
     opt.add_priors()
     opt.add_likelihood()
     start_spread = {"velocity_norm": [-3.0, 3.0]}
+    fit_kwargs = {
+        "rel_tolerance": 0.01,
+        "abs_tolerance": 0.1,
+        "learning_rate": 1e-2,
+    }
     opt.optimize(
         start_spread=start_spread,
         approx=True,
         smc=False,
+        fit_kwargs={
+            "n": 10_000,
+            "rel_tolerance": 0.01,
+            "abs_tolerance": 0.1,
+            "learning_rate": 1e-2,
+        },
+        sample_kwargs={
+            "n_init": 10_000,
+            "init_kwargs": fit_kwargs,
+            "chains": 2,
+            "tune": 100,
+            "draws": 100,
+        },
     )
     assert opt.best_model.n_clouds == 1
     assert opt.null_bic == opt.models[1].null_bic()
@@ -78,10 +119,22 @@ def test_optimize_mcmc():
     opt.add_priors()
     opt.add_likelihood()
     start_spread = {"velocity_norm": [-3.0, 3.0]}
+    fit_kwargs = {
+        "rel_tolerance": 0.01,
+        "abs_tolerance": 0.1,
+        "learning_rate": 1e-2,
+    }
     opt.optimize(
         start_spread=start_spread,
         approx=False,
         smc=False,
+        sample_kwargs={
+            "n_init": 10_000,
+            "init_kwargs": fit_kwargs,
+            "chains": 2,
+            "tune": 500,
+            "draws": 500,
+        },
     )
 
 
@@ -91,13 +144,19 @@ def test_optimize_vi_smc():
     opt.add_priors()
     opt.add_likelihood()
     sample_kwargs = {
-        "chains": 4,
-        "draws": 500,
+        "chains": 2,
+        "draws": 100,
     }
     opt.optimize(
         sample_kwargs=sample_kwargs,
         approx=True,
         smc=True,
+        fit_kwargs={
+            "n": 10_000,
+            "rel_tolerance": 0.01,
+            "abs_tolerance": 0.1,
+            "learning_rate": 1e-2,
+        },
     )
 
 
@@ -107,8 +166,8 @@ def test_optimize_smc():
     opt.add_priors()
     opt.add_likelihood()
     sample_kwargs = {
-        "chains": 4,
-        "draws": 500,
+        "chains": 2,
+        "draws": 100,
     }
     opt.optimize(
         sample_kwargs=sample_kwargs,
